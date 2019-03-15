04fa582ea3981a4cd28debd8cf86c199b29d408a
Quinton <qjohnson@gatech.edu>
2019-03-15 15:28:57 -0400

    Typo and SASS config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: `Quinton Johnson`,
-    description: `My personal portfolio`,
+    description: `My personal portfolio`
   },
   plugins: [
-    `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `./src/images/favicon.png`,
-      },
+        path: `./src/images/favicon.png`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sass`
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,12 +26,12 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `minimal-ui`
         // This path is relative to the root of the site.
-      },
-    },
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
-  ],
+  ]
 };
